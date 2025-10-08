a9e60f9baaa48aacbd21786b3932b4fcd424ef0b
Abbas Ali Dalal <abbas.ali@kadellabs.com>
2025-10-08 12:21:29 +0530

    updated server.ts with remaining port changes

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ import nodemailer from 'nodemailer'
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors({
