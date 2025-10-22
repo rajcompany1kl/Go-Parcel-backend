e900829546ce403c864453f5f64595d673a7198b
Raj Bohara <raj.bohara@kadellabs.com>
2025-10-22 10:40:22 +0530

    Debugged CORS error

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,10 @@ dotenv.config();
 
 const app = express();
 
+
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
 }));
 
