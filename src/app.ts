3e78b7e5eb8058441eba5a03369f96f19c98eba1
Raj Bohara <raj.bohara@kadellabs.com>
2025-10-22 11:06:03 +0530

    Email through API

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import routes from "./routes/index";
-
+import { Resend } from "resend";
 import nodemailer from "nodemailer";
 
 dotenv.config();
@@ -26,26 +26,22 @@ app.get("/", (req: Request, res: Response) => {
 
 // Email route
 
+// initialize resend client
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 app.post("/api/send-email", async (req: Request, res: Response) => {
   const { to, subject, text } = req.body;
 
   try {
-    let transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "rajcompany1kl@gmail.com",
-        pass: process.env.PASS,
-      },
-    });
-
-    await transporter.sendMail({
-      from: '"Delivery System" <rajcompany1kl@gmail.com>',
+    const data = await resend.emails.send({
+      from: "Delivery System <onboarding@resend.dev>", // or use your verified domain
       to,
       subject,
       text,
     });
 
-    res.status(200).json({ message: "Email sent" });
+    console.log("Email sent:", data);
+    res.status(200).json({ message: "Email sent successfully" });
   } catch (err) {
     console.error("Email send failed:", err);
     res.status(500).json({ error: "Email failed to send" });
